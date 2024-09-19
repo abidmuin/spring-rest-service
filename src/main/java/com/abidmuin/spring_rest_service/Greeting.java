66ea3e04906133c2b322aecf7f49203b37b22994
Abid Hasan Muin <muin.739@gmail.com>
2024-09-19 17:14:20 +0600

    v1.0.0

---
+++ src/main/java/com/abidmuin/spring_rest_service/Greeting.java
@@ -0,0 +1,4 @@
+package com.abidmuin.spring_rest_service;
+
+public record Greeting(long id, String content) {
+}
---
+++ src/test/java/com/abidmuin/spring_rest_service/SpringRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abidmuin.spring_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
